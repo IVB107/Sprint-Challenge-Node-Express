9f5ebf4b75fdaf57d82ced77794f81cc4b4bb09d
IVB107 <ianvb107@gmail.com>
2019-03-22 14:59:02 -0500

    Brought stretch react app into project

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const actionRouter = require('./routers/action-router.js');
 const projectRouter = require('./routers/project-router.js');
@@ -10,6 +11,7 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 
 // Routing
 

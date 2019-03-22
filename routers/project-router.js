6122f78294b6fed9ebede8873241f9a7400c9e60
IVB107 <ianvb107@gmail.com>
2019-03-22 12:07:51 -0500

    Added POST method to project-router.js

--- routers/project-router.js
+++ routers/project-router.js
@@ -22,12 +22,34 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  // Stuff
+  db.get(id)
+    .then(project => {
+      return project
+        ? res.status(200).json(project)
+        : res.status(404).json({ message: "No projects with specified ID." });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500);
+    })
 });
+
 
 // POST --> /api/projects
 router.post('/', (req, res) => {
-  // Stuff
+  const { name, description } = req.body;
+  const newProject = req.body;
+
+  return !name || !description
+    ? res.status(400).json({ message: "Please provide the following keys: name, description" })
+    : db.insert(newProject)
+      .then(action => {
+        res.status(200).json(action)
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500);
+      })
 });
 
 // PUT --> /api/projects/:id
